85fd67eabdd51c286761891b0afd056089d09d3d
Alberto <albertomozodocente@gmail.com>
2023-11-07 18:14:34 +0100

    cambios config n

--- client/assets/js/config.js
+++ client/assets/js/config.js
@@ -1,5 +1,5 @@
 
 const url_base = location.hostname + ':' + location.port;
 console.log(url_base);
-const API_URL_BASE =  'http://localhost:3000/'; // DESARROLLO
+const API_URL_BASE =  'https://'+url_base+'/'; // DESARROLLO
 
